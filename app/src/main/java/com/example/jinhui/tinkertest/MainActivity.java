90afc458bbeed74b9740b3f99412fa64c3f414ac
jinhuizxc <1004260403@qq.com>
2017-12-20 18:06:53 +0800

    配置下Tinker热更新/as-2.3.3环境

--- app/src/main/java/com/example/jinhui/tinkertest/MainActivity.java
+++ app/src/main/java/com/example/jinhui/tinkertest/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.jinhui.tinkertest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+/**
+ * 自己集成Tinker ——测试如何使用热更新
+ */
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e(TAG, "onCreate");
     }
 }
